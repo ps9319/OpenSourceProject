7f5ed987a8c23afa3b7c1f1a377925844e979968
kssumin <201566@jnu.ac.kr>
2023-12-12 01:26:45 +0900

    refactor : jpa사용하도록 변경

--- youtubeapi/src/main/java/com/example/youtubeapi/repository/YoutubeVideoRepository.java
+++ youtubeapi/src/main/java/com/example/youtubeapi/repository/YoutubeVideoRepository.java
@@ -1,7 +1,7 @@
 package com.example.youtubeapi.repository;
 
 import com.example.youtubeapi.domain.MyVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface YoutubeVideoRepository {
-    MyVideo save(MyVideo myVideo);
+public interface YoutubeVideoRepository extends JpaRepository<MyVideo, Long> {
 }
