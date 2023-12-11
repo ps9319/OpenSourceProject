ba0dce7574871d9544e173ea4ccfe48764a7e28b
kssumin <201566@jnu.ac.kr>
2023-12-12 01:27:10 +0900

    refactor : 주석 제거

--- youtubeapi/src/main/java/com/example/youtubeapi/service/MyAllVideoService.java
+++ youtubeapi/src/main/java/com/example/youtubeapi/service/MyAllVideoService.java
@@ -11,52 +11,45 @@ import com.google.api.services.youtube.model.VideoListResponse;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import com.google.api.services.youtube.model.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class MyAllVideoService {
 
     private final YoutubeService youtubeService;
     private final YoutubeVideoRepository youtubeVideoRepository;
 
-    @Autowired
-    public MyAllVideoService(YoutubeService youtubeService, YoutubeVideoRepository youtubeVideoRepository) {
-        this.youtubeService = youtubeService;
-        this.youtubeVideoRepository = youtubeVideoRepository;
-
-    }
-
-    public VideoListResponse getAllVideo()
-            throws GeneralSecurityException, IOException, GoogleJsonResponseException{
+    private VideoListResponse getAllVideo()
+            throws GeneralSecurityException, IOException {
         YouTube youtubeServiceEx = youtubeService.getService();
-        // Define and execute the API request
+
         YouTube.Videos.List request = youtubeServiceEx.videos()
                 .list("snippet");
 
-        VideoListResponse response = request.setKey(youtubeService.getDeveloperKey())
+        return request.setKey(youtubeService.getDeveloperKey())
                 .setChart("mostPopular")
                 .setRegionCode("KR")
                 .setMaxResults(50L)
                 .execute();
-        return response;
     }
-    public void putAllVideo(VideoListResponse videoListResponse){
+
+    public void putAllVideo() throws GeneralSecurityException, IOException {
+        VideoListResponse videoListResponse = getAllVideo();
         ArrayList<Video> videos = (ArrayList<Video>) videoListResponse.get("items");
         Iterator<Video> it = videos.iterator();
-        int count = 1;
         while (it.hasNext()) {
             VideoSnippet videoInfo = (VideoSnippet) it.next().get("snippet");
             String videoTitle = videoInfo.getTitle();
-            //System.out.println(count + " = " + videoTitle);
-            MyVideo myVideo = new MyVideo();
-            myVideo.setTitle(videoTitle);
-            myVideo.setId(Long.parseLong(String.valueOf(count)));
-            myVideo.setDate("1");
-            youtubeVideoRepository.save(myVideo);
-            count++;
+
+            MyVideo video = MyVideo.builder()
+                    .title(videoTitle)
+                    .build();
+            youtubeVideoRepository.save(video);
         }
     }
 }
