53ca89fd901e7c24b1c1f3eb047028434c7a8882
kssumin <201566@jnu.ac.kr>
2023-12-16 21:41:07 +0900

    feat : 특정 시간대 데이터 가져오도록 구현

--- youtubeapi/src/main/java/com/example/youtubeapi/repository/YoutubeVideoRepository.java
+++ youtubeapi/src/main/java/com/example/youtubeapi/repository/YoutubeVideoRepository.java
@@ -1,7 +1,13 @@
 package com.example.youtubeapi.repository;
 
 import com.example.youtubeapi.domain.MyVideo;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface YoutubeVideoRepository extends JpaRepository<MyVideo, Long> {
+    @Query("select v from MyVideo v where v.createdDate >= :startDate and v.createdDate <= :endDate")
+    List<MyVideo> findByDate(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
